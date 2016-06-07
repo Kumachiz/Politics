933740e99b989e812401d62f922e9c030bf82ca8
Alex Migdol and Michael Munyaneza <kumachiz@gmail.com>
2016-06-06 20:37:21 -0700

    add final logic for form to give back result of being liberal, moderate, or conservative

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
 
-var choice = $("input:radio[name=choice]:checked").val();
-var death_penalty = $("input:radio[name=death_penalty]:checked").val();
+  $("form#views").submit(function(event) {
+    var choice = $("input:radio[name=choice]:checked").val();
+    var death_penalty = $("input:radio[name=death_penalty]:checked").val();
 
 
-if (results) {
-  if (choice === 'pro-choice' && death_penalty === 'No') {
-    $("#liberal_text").show();
-  }
-  else if (choice === 'pro-choice' && death_penalty === 'Yes')
-    $("#moderate_text").show();
-  } else {
-    $("conservative_text").show();
-  }
+    if (choice === 'pro-choice' && death_penalty === 'No') {
+      $("#liberal_text").show();
+    }
+    else if (choice === 'pro-choice' && death_penalty === 'Yes') {
+      $("#moderate_text").show();
+    } else {
+      $("#conservative_text").show();
+    }
 
-}
 
+  event.preventDefault();
+
+});
 });
